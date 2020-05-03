c07d981dc42a7b4ab6cfe628e264d638116a3572
Iamwafiq <wafiqbandey01@gmail.com>
2020-05-03 21:53:05 +0530

    added aut

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -2,14 +2,15 @@ import React from "react";
 
 
 function LoginPage() {
-  const [count, setCount] = React.useState(0);
+  const [user, setUser] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
+  const submit = () => {
+    if(user=='test' && password=='test1234'){
+      console.log('Success')
+    }else{
+      console.log('Try Again')
+    }
   };
 
   return (
@@ -18,13 +19,13 @@ function LoginPage() {
       
         <tr style={{'height': '30px','margin': 'auto', 'padding': '5px', 'fontSize':'18px'}}>
           <td><label >Username</label></td>
-          <td><input type="text" id="user"/></td>
+          <td><input onBlur={(e)=> setUser(e.target.value)} type="text" id="user"/></td>
         </tr>
         <tr style={{'height': '30px', 'margin': 'auto'}}>
           <td><label >Password</label></td>
-          <td><input type="password" id="pwd"/></td>
+          <td><input onBlur={(e)=> setPassword(e.target.value)} type="password" id="pwd"/></td>
         </tr>
-        <button style={{'margin': '5px 40% 0px', 'backgroundColor': '#85C9E9','color':'white'}}>Login</button>
+        <button onClick={()=>submit()} style={{'margin': '5px 40% 0px', 'backgroundColor': '#85C9E9','color':'white'}}>Login</button>
       </table>
     </div>
   );
